b98592e506545ee9e4c82b8ed74340945116ad78
Eivind Lie Andreassen <ela4991@gmail.com>
2017-11-16 13:31:27 +0100

    Cleaned up FollowCamera

--- Assets/scripts/FollowCamera.cs
+++ Assets/scripts/FollowCamera.cs
@@ -13,17 +13,9 @@ public class FollowCamera : MonoBehaviour
 
     void LateUpdate()
     {
-        float currentX = transform.eulerAngles.x;
-        float desiredX = target.transform.eulerAngles.x;
-        float x = Mathf.LerpAngle(currentX, desiredX, Time.deltaTime * damping);
-
         float currentY = transform.eulerAngles.y;
         float desiredY = target.transform.eulerAngles.y;
         float y = Mathf.LerpAngle(currentY, desiredY, Time.deltaTime * damping);
-        
-        float currentZ = transform.eulerAngles.z;
-        float desiredZ = target.transform.eulerAngles.z;
-        float z = Mathf.LerpAngle(currentZ, desiredZ, Time.deltaTime * damping);
 
         Quaternion rotation = Quaternion.Euler(0, y, 0);
         transform.position = target.transform.position - (rotation * offset);
